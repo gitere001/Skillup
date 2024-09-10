9f4f539ebc32e4a208d0dd6ede10ad4310ebf6b9
James gitere <sasmuypwa2019.com>
2024-09-10 10:14:53 +0300

    update sequelize

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 5000
+import express from 'express';
+import syncDatabase from './src/storage/sychronizeDb';
+
+const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Skillup')
-})
+  res.send('Hello, Skillup!');
+});
 
-app.listen(port, () => {
-    console.log('server started')
-})
+// Start your server after syncing the database
+const startServer = async () => {
+  try {
+    await syncDatabase(); // Sync the database
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
+  } catch (error) {
+    console.error('Error starting the server:', error);
+  }
+};
+
+startServer();
